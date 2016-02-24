5bb4ed114ff5c640bd5e9a94dd2ce6bd8270c053
akht <picopeko@gmail.com>
2016-02-24 21:52:45 +0900

    first commit

---
+++ src/AppFrame.java
@@ -0,0 +1,337 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+@SuppressWarnings("serial")
+public class AppFrame extends JFrame implements ActionListener {
+
+    private Container contentPane = getContentPane();
+
+    private JPanel topPanel;
+    private JPanel centerPanel;
+    private JPanel bottomPanel;
+    private JPanel resultPanel;
+
+    private JLabel questionLabel;
+    private JLabel howManyTimesLabel;
+    private JLabel countLabel;
+    private JLabel resultLabel;
+
+    private JButton btn;
+    private JButton editBtn;
+    private JButton nextBtn;
+    private JButton replayBtn;
+
+    private JTextField inputField;
+
+    private long start;
+    private long end;
+
+    private String fileName;
+    private String targetFile;
+    private ArrayList<String[]> sentenceList;
+    private int index = 0;
+
+    private File logFile;
+    private MyankiLog log;
+
+    public AppFrame() {
+        initComponents();
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new AppFrame().setVisible(true));
+    }
+
+    // スタート画面を構築する
+    private void initComponents() {
+        setTitle("Myanki");
+        setSize(650, 400);
+        setLocationRelativeTo(null);
+        addWindowListener(new MyWindowListener());
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // スタート画面のパネル
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        // スタートボタン
+        // TODO: アニメーションをつける
+        btn = new JButton("Click To Start");
+        btn.setBorderPainted(false);
+        btn.addActionListener(this);
+
+        panel.add(btn, BorderLayout.CENTER);
+        contentPane.add(panel);
+
+        // リザルト画面用のパネルを用意
+        resultPanel();
+
+        // Myanki用のパネルを用意
+        initMyankiPanel();
+    }
+
+    // Myanki用のパネルを生成
+    private void initMyankiPanel() {
+        // sentenceListを用意
+        makeSentenceList();
+        // ログを生成
+        logFile = new File("src/files/myankilog.txt");
+        log = new MyankiLog(logFile);
+
+        // BorderLayout.NORTHに配置するパネル
+        topPanel = new JPanel();
+        topPanel.setLayout(new FlowLayout());
+        topPanel.setPreferredSize(new Dimension(getWidth(), getHeight() / 3));
+
+        // BorderLayout.CENTERに配置するパネル
+        centerPanel = new JPanel();
+        centerPanel.setPreferredSize(new Dimension(getWidth(), getHeight() / 3));
+
+        // BorderLayout.SOUTHに配置するパネル
+        bottomPanel = new JPanel();
+
+
+        // この問題ファイルが何回目かを表示するラベル
+        howManyTimesLabel = new JLabel();
+        howManyTimesLabel.setHorizontalAlignment(JLabel.LEFT);
+
+        // 今やっている問題ファイルをエディタで開くボタン
+        editBtn = new JButton("edit");
+        editBtn.addActionListener(this);
+
+        // 今何問目かを表示するラベル
+        countLabel = new JLabel();
+        countLabel.setHorizontalAlignment(JLabel.RIGHT);
+
+        // 問題文用のラベル
+        questionLabel = new JLabel();
+        questionLabel.setOpaque(true);
+        questionLabel.setBackground(new Color(250, 250, 255));
+        questionLabel.setPreferredSize(new Dimension(getWidth(), 80));
+        questionLabel.addMouseListener(new MyMouseListener());
+        questionLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        // ユーザーが入力するテキストフィールド
+        inputField = new JTextField();
+        Font font = new Font("sanserif", Font.PLAIN, 16);
+        inputField.setFont(font);
+        inputField.setCaretColor(Color.GRAY);
+        inputField.addActionListener(this);
+        inputField.setPreferredSize(new Dimension(500, 45));
+        inputField.setHorizontalAlignment(JTextField.CENTER);
+
+        // 次へボタン
+        nextBtn = new JButton();
+        nextBtn.addActionListener(this);
+        nextBtn.setText("skip");
+        nextBtn.setPreferredSize(new Dimension(45, 45));
+
+
+        topPanel.add(howManyTimesLabel);
+        topPanel.add(editBtn);
+        topPanel.add(countLabel);
+        centerPanel.add(questionLabel);
+        bottomPanel.add(inputField);
+        bottomPanel.add(nextBtn);
+
+    }
+
+    private void setMyankiPanels() {
+        contentPane.add(topPanel, BorderLayout.NORTH);
+        contentPane.add(centerPanel, BorderLayout.CENTER);
+        contentPane.add(bottomPanel, BorderLayout.SOUTH);
+    }
+
+    // リザルト画面用のパネル
+    private void resultPanel() {
+        // リザルト画面のパネル
+        resultPanel = new JPanel();
+        resultPanel.setLayout(new GridLayout(3, 1));
+
+        // RESULTと表示するラベル
+        JLabel titleLabel = new JLabel();
+        titleLabel.setHorizontalAlignment(JLabel.CENTER);
+        titleLabel.setText("RESULT");
+
+        // 実際の結果を表示するラベル
+        resultLabel = new JLabel();
+        resultLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        // リプレイボタン
+        // TODO: アニメーションをつける
+        replayBtn = new JButton("click to replay");
+        replayBtn.setBorderPainted(false);
+        replayBtn.addActionListener(this);
+
+        resultPanel.add(titleLabel);
+        resultPanel.add(resultLabel);
+        resultPanel.add(replayBtn);
+    }
+
+    // 日本語と英文が入ったsentenceListを作成
+    // ([日,英], [日,英], [日,英], ...)となっている
+    private ArrayList<String[]> makeSentenceList() {
+        // 出題に使うテキストファイルを決定
+        String path = "src/res";
+        File dir = new File(path);
+        String[] files = dir.list();
+        int fileIndex = new Random().nextInt(files.length);
+        fileName = files[fileIndex];
+        targetFile = path + "/" + fileName;
+
+        // ファイルの中身を読み込み、ArrayListに格納
+        sentenceList = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(targetFile))) {
+            String line = br.readLine();
+            while (line != null) {
+                sentenceList.add(line.split("\t"));
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sentenceList;
+    }
+
+
+    // myankiPanel上のラベルにテキストをセットする
+    private void setMyankiText() {
+        String questionText = sentenceList.get(index)[0];
+        String countText = String.valueOf(index + 1);
+        String logText = log.getCount(targetFile);
+        questionLabel.setText(questionText);
+        howManyTimesLabel.setText("(" + logText + ")" + fileName);
+        countLabel.setText(countText + "/10");
+    }
+
+    // resultPanel上のラベルにテキストをセットする
+    private void setResultText() {
+        long duration = end - start;
+        String time = String.valueOf(duration / 1000);
+        resultLabel.setText(time + "秒でクリアしました");
+    }
+
+    // inputFieldでイベントが発生したら実行
+    public void checkEvent() {
+        String userInput = inputField.getText();
+        // 入力が"n"なら次の問題へ
+        if (userInput.equals("n")) {
+            nextQestion();
+        }
+
+        // 正誤判定
+        // 正解なら次の問題へ、間違いなら入力文字を全選択
+        if (checkAnswer()) {
+            nextQestion();
+        } else {
+            inputField.selectAll();
+        }
+    }
+
+    // 正誤判定
+    public boolean checkAnswer() {
+        EnglishSentence correct = new EnglishSentence();
+        EnglishSentence user = new EnglishSentence();
+        correct.sentence = sentenceList.get(index)[1];
+        user.sentence = inputField.getText();
+
+        return user.isCorrect(correct);
+    }
+
+    // スタート画面のパネルを削除し、Myanki用のパネルを表示
+    public void startMyanki() {
+        contentPane.removeAll();
+        setMyankiPanels();
+        setMyankiText();
+        contentPane.repaint();
+        inputField.requestFocus();
+        start = System.currentTimeMillis();
+    }
+
+    // リザルト画面を表示
+    public void showResult() {
+        contentPane.removeAll();
+        contentPane.add(resultPanel);
+        setResultText();
+        contentPane.repaint();
+    }
+
+    public void nextQestion() {
+        index++;
+        if (index < 10) {
+            setMyankiText();
+            inputField.setText("");
+            inputField.requestFocus();
+        } else {
+            log.update(fileName);
+            inputField.setText("");
+            end = System.currentTimeMillis();
+            showResult();
+        }
+    }
+
+    public void openInEditor() {
+        File file = new File(targetFile);
+        Desktop desktop = Desktop.getDesktop();
+        try {
+            desktop.open(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btn) {
+            startMyanki();
+        }
+
+        if (e.getSource() == inputField) {
+            checkEvent();
+        }
+
+        if (e.getSource() == replayBtn) {
+            index = 0;
+            startMyanki();
+        }
+
+        if (e.getSource() == nextBtn) {
+            nextQestion();
+        }
+
+        if (e.getSource() == editBtn) {
+            openInEditor();
+        }
+    }
+
+    private class MyMouseListener extends MouseAdapter {
+        @Override
+        public void mousePressed(MouseEvent e) {
+            if (e.getSource() == questionLabel) {
+                questionLabel.setText(sentenceList.get(index)[1]);
+            }
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            if (e.getSource() == questionLabel) {
+                questionLabel.setText(sentenceList.get(index)[0]);
+            }
+        }
+    }
+
+    private class MyWindowListener extends WindowAdapter {
+        @Override
+        public void windowClosing(WindowEvent e) {
+            log.refresh(logFile);
+        }
+    }
+
+}
---
+++ src/EnglishSentence.java
@@ -0,0 +1,111 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EnglishSentence {
+    private static Map<String[], String> shortformsMap;
+    String sentence;
+
+    public EnglishSentence() {
+        // 省略表現と置換用文字列が入ったHashMapを作る
+        String shortformsFile = "src/files/shortforms.txt";
+        shortformsMap = makeShortformsMap(shortformsFile);
+    }
+
+    // 短縮形を置き換えた後の文字列が等しいか調べる
+    public boolean isCorrect(EnglishSentence target) {
+        String correct = formatString(this.sentence);
+        String user = formatString(target.sentence);
+
+        return user.equals(correct);
+    }
+
+    // 5つのステップでテキストを整形する
+    // Step1. テキストの末尾がピリオド(.)なら、ピリオドを除去する
+    // Step2. 引数で受け取ったテキストをloweCaseにする
+    // Step3. テキスト中に丸括弧()があれば、()自身と()で囲まれた部分を除去する
+    // Step4. Step3でできたテキストを整形(空白文字が2つ以上あれば1つに置き換える)
+    // Step5. 短縮形とその原形をREPLACED(n)という文字列に置き換える
+    // TODO: Step3の動作を変える。丸括弧の中身と直前の単語のどちらでも正解と判定できるように
+    private String formatString(String targetString) {
+        // Step1, Step2
+        String returnStr = removePeriod(targetString).toLowerCase();
+
+        // Step3
+        String pRegex = "\\(.*?\\)";
+        returnStr = returnStr.replaceAll(pRegex, "");
+
+        // Step4
+        String sRegex = "\\s{2,}";
+        returnStr = returnStr.replaceAll(sRegex, " ");
+
+        // Step5
+        // HashMapのキー(配列になっている)から正規表現オブジェクトを作って
+        // それにマッチした部分をキーに対応する値(REPLACED(n))に置換
+        // どれにもマッチしなければ引数で受け取った文字列を小文字にして返す
+        for (Entry<String[], String> entry : shortformsMap.entrySet()) {
+            String[] key = entry.getKey();
+            String value = entry.getValue();
+
+            for (String shortf : key) {
+                Pattern p = Pattern.compile(shortf);
+                Matcher m = p.matcher(returnStr);
+                if (m.find()) {
+                    returnStr = m.replaceAll(value);
+                }
+            }
+        }
+
+        return returnStr;
+    }
+
+    // shortforms.txtからshortformsMapを作る
+    // shortformsMapは次のような構造になっている
+    // key: ["It's", "It is"]
+    // value: "REPLACED1"
+    private Map<String[], String> makeShortformsMap(String filename) {
+        Map<String[], String> map = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line = br.readLine();
+            while (line != null) {
+                for (int i = 0; i < countLines(filename); i++) {
+                    String[] key = line.split("\t");
+                    String value = "REPLACED" + i;
+                    map.put(key, value);
+                    line = br.readLine();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+    // 受け取ったStringの末尾が.(ピリオド)で終わっていれば、
+    // .(ピリオド)を削除して返す
+    private String removePeriod(String str) {
+        if (str.endsWith(".")) {
+            return str.substring(0, str.length() - 1);
+        }
+        return str;
+    }
+
+    // テキストファイルの行数を返す
+    private static int countLines(String filename) {
+        int counts = 0;
+        try (LineNumberReader lnr = new LineNumberReader(new FileReader(filename))) {
+            lnr.skip(Integer.MAX_VALUE);
+            counts = lnr.getLineNumber() + 1;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return counts;
+    }
+}
