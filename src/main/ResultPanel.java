8bbefc2dbf359512f3371a0ed4f0f6d1c0544f95
akht <picopeko@gmail.com>
2016-04-08 00:42:06 +0900

    remove unused import

--- src/main/ResultPanel.java
+++ src/main/ResultPanel.java
@@ -2,8 +2,6 @@ package main;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class ResultPanel extends JPanel {
     private AppFrame appFrame;
