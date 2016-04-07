451503a5cd487a63c4709267b299a60785cf3752
akht <picopeko@gmail.com>
2016-04-07 20:01:39 +0900

    improve result screen

--- src/main/ResultPanel.java
+++ src/main/ResultPanel.java
@@ -16,29 +16,39 @@ public class ResultPanel extends JPanel {
         setSize(AppFrame.APP_WIDTH, AppFrame.APP_HEIGHT);
         setLayout(new BorderLayout());
 
-        // ResultPanelの上にresultTopPanel,resultBottomPanelを配置し
-        // その２枚のパネルの上に各コンポーネントを配置する
+        // ResultPanelの上にresultTopPanel,resultBottomPanelを配置
+        // topのほうにはresultLabelを配置
+        // bottomのほうにはさらに２枚のパネルを配置し
+        // そのパネル上にラベルやボタンを配置する
+
         JPanel resultTopPanel = new JPanel();
-
-        JPanel resultBottomPanel = new JPanel();
-        resultBottomPanel.setLayout(new FlowLayout());
-
         // クリアにかかった時間を表示するラベル
         resultLabel = new JLabel();
         resultLabel.setHorizontalAlignment(JLabel.CENTER);
+        resultTopPanel.add(resultLabel);
 
+        JPanel resultBottomPanel = new JPanel();
+        resultBottomPanel.setLayout(new GridLayout(2,0));
+
+        JPanel panelForLabel = new JPanel();
+        JLabel replayLabel = new JLabel("----------- リプレイ -----------");
+        replayLabel.setForeground(Color.GRAY);
+        replayLabel.setHorizontalAlignment(JLabel.CENTER);
+        panelForLabel.add(replayLabel);
+
+        JPanel panelForBtns = new JPanel();
         // 同じ問題をもう一度
         JButton replayBtn = new JButton("同じ問題");
         replayBtn.addActionListener(e -> appFrame.game.replay());
+        panelForBtns.add(replayBtn);
 
         // 違う問題へ
-        JButton randomPlayBtn = new JButton("違う問題");
+        JButton randomPlayBtn = new JButton("ランダム");
         randomPlayBtn.addActionListener(e -> appFrame.game.replayRandomly());
+        panelForBtns.add(randomPlayBtn);
 
-
-        resultTopPanel.add(resultLabel);
-        resultBottomPanel.add(replayBtn);
-        resultBottomPanel.add(randomPlayBtn);
+        resultBottomPanel.add(panelForLabel);
+        resultBottomPanel.add(panelForBtns);
 
         this.add(resultTopPanel, BorderLayout.CENTER);
         this.add(resultBottomPanel, BorderLayout.SOUTH);
